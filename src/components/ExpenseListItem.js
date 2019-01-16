bf301d757a282a41c32f517e3dcfdfc25fcfdd1f
Thiii <fireter.trongthi@gmail.com>
2019-01-16 06:13:06 +0200

    Set up formatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -5,11 +5,18 @@ import React from 'react';
 import {Link} from 'react-router-dom'; // you have to use LINK in order to remain the ID for expense. a href automatically refresh new ID 
 import {connect} from 'react-redux';
 import { removeExpense } from '../actions/expenses'
+import moment from 'moment';
+import numeral from '../style/numeral';
+
 export const ExpenseListItem = ({id, dispatch, description, amount, createdAt}) => (
     <div>
         <Link to= {`/edit/${id}`}>
         <h3>{description}</h3></Link> 
-        <p>{amount} - {createdAt}</p>
+        <p>
+        {numeral(amount)}  {` `}
+        - 
+        {` `}{moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
         <button onClick={() => {
             dispatch(removeExpense({id}))
         }} >Remove</button>
